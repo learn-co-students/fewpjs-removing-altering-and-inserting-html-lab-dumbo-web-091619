cea5e119a826c7be798199e1f0f417832a11a3d7
markdenardo <mark.denardo@gmail.com>
2019-10-28 18:06:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-// Write your code here!
-const testVar = "hi"
-
-/* Enter the code to remove the node element here */
-document.querySelector("main#main").remove();
-
-/* Create your new element here and assign it to newHeader */
-const newHeader = document.createElement("h1");
+let newHeader = document.createElement('h1')
 newHeader.id = "victory"
-newHeader.innerHTML = "Flatiron School is the champion!";
-newHeader.className = "victory";
+let name = "YOUR-NAME is the champion"
+newHeader.innerHTML = name
+document.querySelector('main#main').remove()
